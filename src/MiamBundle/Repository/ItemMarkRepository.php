4c655fe42e0e4174b89ee521e484e950c2fc8a70
Yuzmi <mau24@laposte.net>
2016-05-01 10:37:32 +0200

    Unstar items when unsubscribe

--- src/MiamBundle/Repository/ItemMarkRepository.php
+++ src/MiamBundle/Repository/ItemMarkRepository.php
@@ -2,6 +2,7 @@
 
 namespace MiamBundle\Repository;
 
+use MiamBundle\Entity\Feed;
 use MiamBundle\Entity\User;
 
 class ItemMarkRepository extends \Doctrine\ORM\EntityRepository
@@ -12,5 +13,18 @@ class ItemMarkRepository extends \Doctrine\ORM\EntityRepository
 			->where("m.user = :user")->setParameter("user", $user)
 			->andWhere("m.isStarred = TRUE")
 			->getQuery()->getSingleScalarResult();
+	}
+
+	public function findStarredForFeedAndUser(Feed $feed, User $user) {
+		return $this->createQueryBuilder("m")
+			->innerJoin("m.item", "i")
+			->where("m.user = :user")
+			->andWhere("m.isStarred = TRUE")
+			->andWhere("i.feed = :feed")
+			->setParameters(array(
+				'feed' => $feed,
+				'user' => $user
+			))
+			->getQuery()->getResult();
 	}
 }
