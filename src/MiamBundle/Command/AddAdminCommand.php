4366488ccfc5dea527e0b5031a11c3e2038bf0c2
Yuzmi <mau24@laposte.net>
2016-06-28 03:22:45 +0200

    Commands to add or remove admins

---
+++ src/MiamBundle/Command/AddAdminCommand.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace MiamBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class AddAdminCommand extends ContainerAwareCommand {
+	protected function configure() {
+        $this
+            ->setName('miam:admin:add')
+            ->setDescription("Add an admin")
+            ->addArgument('user', InputArgument::REQUIRED)
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output) {
+        error_reporting(0);
+
+        $userArgument = $input->getArgument('user');
+
+        $em = $this->getContainer()->get('doctrine')->getEntityManager();
+
+        $user = $em->getRepository('MiamBundle:User')->findOneByUsername($userArgument);
+        if(!$user) {
+            $output->writeln('User not found.');
+        } elseif($user->getIsAdmin()) {
+            $output->writeln($user->getUsername().' is already admin.');
+        } else {
+            $user->setIsAdmin(true);
+            $em->persist($user);
+            $em->flush();
+
+            $output->writeln($user->getUsername().' is now admin.');
+        }
+    }
+}
---
+++ src/MiamBundle/Command/RemoveAdminCommand.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace MiamBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class RemoveAdminCommand extends ContainerAwareCommand {
+	protected function configure() {
+        $this
+            ->setName('miam:admin:remove')
+            ->setDescription("Remove an admin")
+            ->addArgument('user', InputArgument::REQUIRED)
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output) {
+        error_reporting(0);
+
+        $userArgument = $input->getArgument('user');
+
+        $em = $this->getContainer()->get('doctrine')->getEntityManager();
+
+        $user = $em->getRepository('MiamBundle:User')->findOneByUsername($userArgument);
+        if(!$user) {
+            $output->writeln('User not found.');
+        } elseif(!$user->getIsAdmin()) {
+            $output->writeln($user->getUsername().' is not admin.');
+        } else {
+            $user->setIsAdmin(false);
+            $em->persist($user);
+            $em->flush();
+
+            $output->writeln($user->getUsername().' is not admin anymore.');
+        }
+    }
+}
