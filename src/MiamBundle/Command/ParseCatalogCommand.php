b93adeb3f7b0e36f61a4d12ba26c82862c606c7d
Yuzmi <mau24@laposte.net>
2016-07-01 01:43:48 +0200

    ParseCatalogCommand

---
+++ src/MiamBundle/Command/ParseCatalogCommand.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace MiamBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ParseCatalogCommand extends ContainerAwareCommand {
+	protected function configure() {
+        $this
+            ->setName('miam:parse:catalog')
+            ->setDescription('Parse all feeds in catalog')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output) {
+        error_reporting(0);
+
+        $output->writeln('Fetching and parsing...');
+
+        $time_begin = time();
+
+        $em = $this->getContainer()->get('doctrine')->getEntityManager();
+        
+        $feeds = $em->getRepository('MiamBundle:Feed')
+            ->createQueryBuilder('f')
+            ->where('f.isCatalog = TRUE')
+            ->getQuery()->getResult();
+
+        $nb = 0;
+        foreach($feeds as $feed) {
+            $feed = $em->getRepository('MiamBundle:Feed')->find($feed->getId());
+
+            if($feed) {
+                $this->getContainer()->get('data_parsing')->parseFeed($feed, array('verbose' => true));
+
+                $nb++;
+            }
+
+            if($nb%20 == 0) {
+                $em->clear();
+            }
+        }
+
+        $duration = time() - $time_begin;
+
+        $output->writeln('');
+        $output->writeln('End - Duration: '.$duration.'s');
+    }
+}
