fe0862c66e29ea3445e3b94b6f0c190590965fa2
Yuzmi <mau24@laposte.net>
2016-10-23 00:27:50 +0200

    Change ignore-invalid option

--- src/MiamBundle/Command/ParseFeedsCommand.php
+++ src/MiamBundle/Command/ParseFeedsCommand.php
@@ -71,7 +71,7 @@ class ParseFeedsCommand extends ContainerAwareCommand {
                     continue;
                 }
 
-                if($input->getOption('ignore-invalid') && $feed->getErrorCount() > 0) {
+                if($input->getOption('ignore-invalid') && $feed->getErrorCount() > 5) {
                     continue;
                 }
 
