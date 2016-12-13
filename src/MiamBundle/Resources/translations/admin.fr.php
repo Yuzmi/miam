141409edac286762d659ac18b5ddb800d8e9d259
Yuzmi <mau24@laposte.net>
2016-12-13 20:03:21 +0100

    fix a bug when parsing in admin

--- src/MiamBundle/Resources/translations/admin.fr.php
+++ src/MiamBundle/Resources/translations/admin.fr.php
@@ -2,5 +2,6 @@
 	return array(
 		'Add to catalog' => 'Ajouter au catalogue',
 		'Remove from catalog' => 'Retirer du catalogue',
-		'Parse' => 'Récupérer'
+		'Parse' => 'Récupérer',
+		'Parsing' => 'Récupération'
 	);
